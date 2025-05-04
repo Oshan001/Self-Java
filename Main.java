c62e4d4ae3f02745972da81d8cf347f3e5063b34
OshanBajrachacharya <oshanbaj11@gmail.com>
2025-05-04 16:39:52 +0545

    first java code

---
+++ Main.java
@@ -0,0 +1,22 @@
+//public class Main{
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        byte a = 1;
+//        char c ='d';
+//        float f =   3.14f;
+//        double d =   3.14d;
+//        int i =   1;
+//        long s=   182448476557878787L;
+//        short aa=33;
+//        boolean bb =true;
+//
+//       System.out.println(a);
+//
+//
+//    }
+//}
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
