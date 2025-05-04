d5f630b1c8d0d4180b3d9f514db8c1453dfe264c
OshanBajrachacharya <oshanbaj11@gmail.com>
2025-05-04 21:30:15 +0545

    practice2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+//SELF JAVA LEARN WITH @CodeWithHarry
+
 //public class Main{
 //    public static void main(String[] args) {
 //        System.out.println("Hello World!");
@@ -15,8 +17,33 @@
 //
 //    }
 //}
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+
+
+
+
+//practice 2
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args) {
+//        System.out.println("User Input");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Your Number:");
+//          int a= sc.nextInt();
+//        System.out.println("your number is "+a);
+//        boolean b= sc.nextBoolean();
+//        boolean c= sc.hasNextBoolean();
+//        int temp1=2;
+//        System.out.println("Enter the character");
+//         char c1= sc.next().charAt(temp1);
+//        System.out.println("the character in"+ temp1+" index is "+c1);
+//        System.out.printf("the character  in %d  index is %c",temp1,c1);
+//        System.out.println("enter the string");
+//        String s1=sc.nextLine();
+//        System.out.println("Enter string1");
+//        System.out.println("input is "+s1);
+//        String s2="os q"+sc.next().charAt(temp1)+" an";
+//        System.out.println("enter string2");
+//        System.out.println("string is "+s2);
+//
+//     }
+//}
